cfd5f8c9ee073b1e6cea7cbd499f9de48cde4469
Armando233-abc <armandovigliotti5@gmail.com>
2020-11-19 21:00:09 +0100

    funzioni js

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 titolo = document.getElementById("titolo")
+var elementi = ["X", "O"]
+td = document.getElementsByTagName("td")
 
+
+td[0].addEventListener("click", () => {
+    var new_x = document.createElement("h1")
+    var t = document.createTextNode(elementi[0])
+    new_x.appendChild(t)
+    new_x.style.textAlign = "center"
+    td[0].appendChild(new_x)
+})
