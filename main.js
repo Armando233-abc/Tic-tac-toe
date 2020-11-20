c7dee9a36c1db2bf82fa7c51eb76337c78620553
Armando233-abc <armandovigliotti5@gmail.com>
2020-11-20 21:29:02 +0100

    aggiunta di forEach

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 titolo = document.getElementById("titolo")
 var elementi = ["img/x.png", "img/o.png"]
-td = document.getElementsByTagName("td")
+td = document.querySelectorAll("td")
 turno = "x"
 indice = 0
 
@@ -35,52 +35,11 @@ function azioni(t){
 }
 
 
-td[0].addEventListener("click", function x(){
-    azioni(td[0])
-    td[0].removeEventListener("click", x)
-})
+td.forEach(element => {
+    element.addEventListener("click", function giocata(){
+        azioni(element)
+        element.removeEventListener("click", giocata)
+    })
+});
 
-td[1].addEventListener("click", function x(){
-    azioni(td[1])
-    td[1].removeEventListener("click", x)
-})
 
-td[2].addEventListener("click", function x(){
-    azioni(td[2])
-    td[2].removeEventListener("click", x)
-})
-
-td[3].addEventListener("click", function x(){
-    azioni(td[3])
-    td[3].removeEventListener("click", x)
-})
-
-td[4].addEventListener("click", function x(){
-    azioni(td[4])
-    td[4].removeEventListener("click", x)
-})
-
-td[5].addEventListener("click", function x(){
-    azioni(td[5])
-    td[5].removeEventListener("click", x)
-})
-
-td[6].addEventListener("click", function x(){
-    azioni(td[6])
-    td[6].removeEventListener("click", x)
-})
-
-td[7].addEventListener("click", function x(){
-    azioni(td[7])
-    td[7].removeEventListener("click", x)
-})
-
-td[8].addEventListener("click", function x(){
-    azioni(td[8])
-    td[8].removeEventListener("click", x)
-})
-
-td[9].addEventListener("click", function x(){
-    azioni(td[9])
-    td[9].removeEventListener("click", x)
-})
